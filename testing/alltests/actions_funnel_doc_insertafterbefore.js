b7d14fc54febcdd3833f7afc4cf09ef99d67303c
Jason Scherer <jason.scherer@gmail.com>
2022-05-20 16:44:38 -0400

    fixed more tests

--- testing/alltests/actions_funnel_doc_insertafterbefore.js
+++ testing/alltests/actions_funnel_doc_insertafterbefore.js
@@ -82,6 +82,8 @@ testactions.push({type:'keydown',code:'ShiftRight'});
 testactions.push({type:'keydown',code:'Digit3'});
 testactions.push({type:'keyup',code:'Digit3'});
 testactions.push({type:'keyup',code:'ShiftRight'});
+testactions.push({type:'keydown',code:'Enter'});
+testactions.push({type:'keyup',code:'Enter'});
 
 const experiment_flags = {
 "DISABLE_ALERT_ANIMATIONS":true,
